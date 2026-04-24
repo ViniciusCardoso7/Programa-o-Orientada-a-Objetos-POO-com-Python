aeacd8ca65784aea2793f166c1840edd0a8d004c
Vinicius Cardoso <132711111+ViniciusCardoso7@users.noreply.github.com>
2026-04-24 11:09:04 -0300

    Herança

---
+++ Programação Orientada a Objetos/CLasses e Objetos/Herança/01_herança_simples.py
@@ -0,0 +1,30 @@
+class Veiculo:
+    def __init__(self,cor,placa,numero_rodas):
+            self.numero_rodas = 0
+            self.placa= placa
+            self.cor = cor
+    def ligar_motor(self):
+        print("Ligando Motor")
+    def __str__(self):
+         return self.cor
+class Motocicleta (Veiculo):
+    pass
+class Carro (Veiculo):
+    pass
+
+class Caminhao (Veiculo):
+    def __init__(self, carregado):
+         super().__init__(cor,placa,numero_rodas)
+         self.carregado = carregado
+    def esta_carregado(self):
+         print(f"{'Sim' if carregado else 'Não'} está carregado")
+
+moto = Motocicleta("Vermelha","ABC-1234",2)
+moto.ligar_motor()
+
+carro = Carro("Preto","DEF-5678",4)
+carro.ligar_motor()
+
+caminhao = Caminhao("Branco","GHI-9012",6,True)
+caminhao.ligar_motor()
+caminhao.esta_carregado()
