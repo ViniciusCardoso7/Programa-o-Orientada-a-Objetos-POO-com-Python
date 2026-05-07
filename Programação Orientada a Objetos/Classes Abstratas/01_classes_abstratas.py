05ccf37ba08d5d8603583182837ebb70c6a9b66d
Vinicius Cardoso <132711111+ViniciusCardoso7@users.noreply.github.com>
2026-05-07 17:43:44 -0300

    Interfaces

---
+++ Programação Orientada a Objetos/Classes Abstratas/01_classes_abstratas.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+class ControleRemoto:
+    @abstractmethod
+    def ligar (self):
+        pass
+    @abstractmethod
+    def desligar(self):
+        pass
+class ControleRemotoTv(ControleRemoto):
+    def ligar(self):
+        print("Ligando a TV")
+    def desligar(self):            
+        print("Desligando a TV")
